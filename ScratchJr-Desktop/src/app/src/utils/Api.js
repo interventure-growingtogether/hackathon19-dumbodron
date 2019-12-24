16489846e70efe9aeb157f36785ef219bfebadce
Arezina <Milos.Arezina@escriba.de>
2019-12-24 11:24:02 +0100

    try post request

---
+++ ScratchJr-Desktop/src/app/src/utils/Api.js
@@ -0,0 +1,38 @@
+export default class Api {
+  url = 'localhost:8080/tello/execute';
+  static requestFromServer (pos, whenDone) {
+    console.log('requestFromServer', pos, this.url);
+    var xmlrequest = new XMLHttpRequest();
+    xmlrequest.addEventListener('error', transferFailed, false);
+    xmlrequest.onreadystatechange = function () {
+      if (xmlrequest.readyState == 4) {
+        whenDone(pos, xmlrequest.responseText);
+      }
+    };
+    xmlrequest.open('GET', this.url, true);
+    xmlrequest.send(null);
+    function transferFailed (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        // Failed loading
+    }
+  }
+  static postRequest (pos, data, whenDone) {
+    //var params = 'orem=ipsum&name=binny';
+    console.log('requestFromServer', pos, this.url, data);
+    var xmlrequest = new XMLHttpRequest();
+    xmlrequest.addEventListener('error', transferFailed, false);
+    xmlrequest.onreadystatechange = function () {
+      if (xmlrequest.readyState == 4) {
+        whenDone(pos, xmlrequest.responseText);
+      }
+    };
+    xmlrequest.open('POST', this.url, true);
+    xmlrequest.send(data);
+    function transferFailed (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        // Failed loading
+    }
+  }
+}
