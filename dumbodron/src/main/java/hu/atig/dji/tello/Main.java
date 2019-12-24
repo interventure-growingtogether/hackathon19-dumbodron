031a50f845a091dc7921164437e318e5ed8523f5
Avramovic Slavisa <avramovic.slavisa@partake.de>
2019-12-24 10:06:02 +0100

    initial tello dron driver thanks Grof Atilla

---
+++ dumbodron/src/main/java/hu/atig/dji/tello/Main.java
@@ -0,0 +1,29 @@
+package hu.atig.dji.tello;
+
+import static java.lang.Thread.sleep;
+
+import hu.atig.dji.tello.model.TelloFlip;
+import hu.atig.dji.tello.world.TelloWorld;
+import hu.atig.dji.tello.world.TelloWorldImpl;
+
+public class Main {
+
+  public static void main(String[] args) {
+    TelloWorld telloWorld = new TelloWorldImpl();
+
+    telloWorld.connect();
+    telloWorld.enterCommandMode();
+    telloWorld.takeOff();
+    telloWorld.doFlip(TelloFlip.backLeft);
+
+    try {
+      sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    telloWorld.land();
+
+  }
+
+}
