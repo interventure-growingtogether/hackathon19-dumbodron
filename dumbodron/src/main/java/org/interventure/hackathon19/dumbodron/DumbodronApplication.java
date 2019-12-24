5f3f22df114b51c4fe401470b091692a272bd7ea
Avramovic Slavisa <avramovic.slavisa@partake.de>
2019-12-24 10:01:08 +0100

    initial server

---
+++ dumbodron/src/main/java/org/interventure/hackathon19/dumbodron/DumbodronApplication.java
@@ -0,0 +1,13 @@
+package org.interventure.hackathon19.dumbodron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbodronApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DumbodronApplication.class, args);
+  }
+
+}
---
+++ dumbodron/src/test/java/org/interventure/hackathon19/dumbodron/DumbodronApplicationTests.java
@@ -0,0 +1,13 @@
+package org.interventure.hackathon19.dumbodron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumbodronApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
