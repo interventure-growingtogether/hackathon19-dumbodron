f24fa9d845fbb5ca89ef61af4d40873b369214f6
Predrag Maric <predrag4@gmail.com>
2019-12-24 11:18:52 +0100

    Added implementations for back, left, right, rotateRight, rotateLeft

--- dumbodron/src/main/java/hu/atig/dji/tello/world/DumboTelloWorldImpl.java
+++ dumbodron/src/main/java/hu/atig/dji/tello/world/DumboTelloWorldImpl.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import hu.atig.dji.tello.model.command.BasicTelloCommand;
 import hu.atig.dji.tello.model.command.TelloCommand;
+import hu.atig.dji.tello.model.command.TelloCommandValues;
 
 public class DumboTelloWorldImpl extends TelloWorldImpl {
 
@@ -15,7 +16,52 @@ public class DumboTelloWorldImpl extends TelloWorldImpl {
 
 	@Override
 	public void forward(Integer distance) {
-		TelloCommand command = new BasicTelloCommand("forward " + distance);
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.FORWARD + " " + distance);
+		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
+		if (executionSuccessful) {
+			logger.info("Forward command was executed successfully");
+		}
+	}
+
+	@Override
+	public void backward(Integer distance) {
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.BACK + " " + distance);
+		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
+		if (executionSuccessful) {
+			logger.info("Back command was executed successfully");
+		}
+	}
+
+	@Override
+	public void right(Integer distance) {
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.RIGHT + " " + distance);
+		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
+		if (executionSuccessful) {
+			logger.info("Right command was executed successfully");
+		}
+	}
+
+	@Override
+	public void left(Integer distance) {
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.LEFT + " " + distance);
+		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
+		if (executionSuccessful) {
+			logger.info("Left command was executed successfully");
+		}
+	}
+
+	@Override
+	public void rotatateRight(Integer angle) {
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.CW + " " + angle);
+		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
+		if (executionSuccessful) {
+			logger.info("Forward command was executed successfully");
+		}
+	}
+
+	@Override
+	public void rotateLeft(Integer angle) {
+		TelloCommand command = new BasicTelloCommand(TelloCommandValues.CCW + " " + angle);
 		boolean executionSuccessful = getTelloCommunication().executeCommand(command);
 		if (executionSuccessful) {
 			logger.info("Forward command was executed successfully");
